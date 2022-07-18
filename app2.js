c0d717ae9eebafd833250f0f46f7031c56d84cb3
Arman <arm.nnn04@gmail.com>
2022-07-19 01:58:42 +0600

    code was fully rewrited

---
+++ app2.js
@@ -0,0 +1,33 @@
+let timeElem = document.querySelector('#time-block');
+let selectElem = document.querySelector('select');
+
+const timeZones = {
+    'Asia/Aqtau': 'Asia/Aqtau',
+    'Asia/Almaty': 'Asia/Almaty',
+    'America/Los_Angeles': 'America/Los_Angeles',
+    'America/New_York': 'America/New_York',
+    'Europe/Kiev': 'Europe/Kiev',
+    'Europe/London': 'Europe/London',
+    'Asia/Hong_Kong': 'Asia/Hong_Kong',
+    'Europe/Istanbul': 'Europe/Istanbul',
+}
+
+const selectedCity = () => {
+    for (key in timeZones) {
+        if (key == selectElem.value) {
+            return key
+        } else {
+            continue
+        }
+    }
+}
+
+const showTime = () => {
+    timeElem.innerHTML = new Date().toLocaleString('en-us', {
+        timeZone: `${selectedCity()}`,
+        timeStyle: 'medium',
+        hourCycle: 'h23'
+    })
+}
+
+setInterval(showTime, 1000)
